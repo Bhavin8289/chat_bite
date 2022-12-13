b70182ff0bb33fdd3070718cb6867b2757555b47
Bhavin8289 <bupadhyay277@gmail.com>
2022-12-13 18:32:58 +0530

    Chatebit New Project

---
+++ android/app/src/main/kotlin/com/example/chat_bite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_bite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chat_bite/Screen/Homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'ChatBite',
+      home: HomePage(),
+    );
+  }
+}
