3f44ef472bce6a6ed6b824e8d4e8097cae852ba8
Bhavin8289 <bupadhyay277@gmail.com>
2022-12-14 14:09:37 +0530

    adding Textfield

--- lib/Screen/Appscreen/Loading.dart
+++ lib/Screen/Appscreen/Loading.dart
@@ -8,10 +8,54 @@ class LoadingPage extends StatefulWidget {
 }
 
 class _LoadingPageState extends State<LoadingPage> {
+  TextEditingController fullNameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey.shade200,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Column(
+              children: const [
+                Image(
+                  image: AssetImage('assets/Gif/Loading.gif'),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(14),
+              child: TextField(
+                controller: fullNameController,
+                decoration: const InputDecoration(
+                  hintText: 'Add Your Intress (Optional)',
+                  // labelText: 'Add Your Intress',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+            Column(
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text("Add"),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
